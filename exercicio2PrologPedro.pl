461760837e76b9baa465b4a2a8e34869f334e285
PedroCassenote <79975977+PedroCassenote@users.noreply.github.com>
2022-09-19 10:22:50 -0300

    Add files via upload

---
+++ exercicio2PrologPedro.pl
@@ -0,0 +1,51 @@
+
+% Fatos
+
+regiao(norte).
+regiao(sul).
+regiao(centro-oeste).
+regiao(leste).
+regiao(oeste).
+
+raca(orcs).
+raca(anoes).
+raca(elfos).
+raca(humanos).
+raca(hobbits).
+
+profissao(guerreiro).
+profissao(rei).
+profissao(mago).
+profissao(ferreiro).
+profissao(ladrao).
+
+lingua(orcs, orces).
+lingua(elfos, elfico).
+lingua(elfos, portugues).
+lingua(anoes, portugues).
+lingua(humanos, portugues).
+lingua(hobbits, portugues).
+
+povoado(condado, leste).
+povoado(valfenda, centro-oeste).
+povoado(mordor, norte).
+povoado(rohan, oeste).
+
+povo(hobbits, condado).
+povo(elfos, valfenda).
+povo(orcs, mordor).
+povo(humanos, rohan).
+
+%personagem(nome, profissao, raca, povoado).
+personagem(gandalf, mago, humano, valfenda).
+personagem(bilbo, ladrao, hobbit, condado).
+personagem(frodo, ladrao, hobbit, condado).
+personagem(legolas, guerreiro, elfo, valfenda).
+personagem(galadriel, mago, elfo, valfenda).
+personagem(gimmly, _, anao, _).
+personagem(boromir, _, humano, rohan).
+
+% Regras
+
+
+
